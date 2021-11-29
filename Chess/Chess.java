c1c46c6d4ce3d8bf872e28afb5e0e1a977411f18
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2021-11-29 15:20:08 +0200

    Update Chess.java

--- Chess/Chess.java
+++ Chess/Chess.java
@@ -1,14 +1,17 @@
 package Chess;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class Chess {
+public class Chess extends JFrame {
     public static Piece[][] board;
     public static LinkedList<Piece> blackPieces, whitePieces;
     Random rand;
+    static JLabel jLabel;
     public void initialSetup(){
         blackPieces = new LinkedList<>();
         whitePieces = new LinkedList<>();
@@ -31,19 +34,51 @@ public class Chess {
             }
             piecesList.addAll(Arrays.asList(board[row]));
         }
+        JFrame window = new JFrame("Chess");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(new BorderLayout());
+        jLabel = new JLabel(boardToStr());
+        window.add(jLabel, BorderLayout.CENTER);
+        window.pack();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
     }
 
 
-    private void displayBoard(){
-        StringBuilder sb = new StringBuilder("\n\n|");
+    private String boardToStr(){
+        StringBuilder sb = new StringBuilder("<html><br><br>|");
         for (Piece[] pieces : board) {
             for (Piece piece : pieces) {
-                sb.append(piece == null ? "\u2007\u2009\u2009" : piece.getImg()).append('|');
+                sb.append(piece == null ? "\u2007\u2007" : piece.getImg()).append('|');
             }
-            sb.append("\n----------------------\n|");
+            sb.append("<br>-------------------------------<br>|");
         }
         sb.deleteCharAt(sb.length()-1);
-        System.out.println(sb);
+        sb.append("</html>");
+        return sb.toString();
+    }
+    private Piece findPiece(Color color){
+        var pieces = color.equals(Color.black) ? blackPieces : whitePieces;
+        Piece piece;
+        do {
+            piece = pieces.get(rand.nextInt(pieces.size()));
+        } while (piece.getPossibleDestinations(board).size() == 0);
+        return piece;
     }
 
+    public static void main(String[] args) throws InterruptedException {
+        var game = new Chess();
+        game.rand = new Random();
+        game.initialSetup();
+        var turn = Color.black;
+        while(true) {
+            jLabel.setText(game.boardToStr());
+            var piece = game.findPiece(turn);
+            var dests = piece.getPossibleDestinations(board);
+            var randomDest = dests.get(game.rand.nextInt(dests.size()));
+            piece.move(randomDest);
+            turn = turn.equals(Color.black) ? Color.white : Color.black;
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
 }
